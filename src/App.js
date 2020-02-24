ea31850d5f3b2db3f3064dc3ec552ac2a014e59a
stadnikivan <39565192+stadnikivan@users.noreply.github.com>
2020-02-24 22:24:50 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            export default class App extends React.Component({
+            render: function() {
+                return (
+                    <div className="main">
+                        <div className="nav">
+                            <button className="butt-nav">Почта</button>
+                            <button className="butt-nav">Картинки</button>
+                            <button className="butt-nav"><img src="application.jpg" /></button>
+                            <button className="butt-nav"><img src="bell.jpg" /></button>
+                            <button className="butt-nav"><img src="m.jpg" /></button>  
+                        </div>
+                        <div>
+                            <img src="logo.jpg" />
 
-export default App;
+                            <input 
+                                type="text" 
+                                placeholder="Search..." 
+                                className="H"
+                                onkeyup="var yratext=/['0-9',':']/; if(yratext.test(this.value)) this.value=''"
+                             />
+
+                        </div>
+                        <div className="footer">
+                            <div className="left">
+                                <button className="butt">реклама</button>
+                                <button className="butt">для бизнеса</button>
+                                <button className="butt">все о Google</button>
+                            </div>
+                            <div className="right">
+                                <button className="butt">конфеденциальность</button>
+                                <button className="butt">Условия</button>
+                                <button className="butt">Настройки</button>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+        });
